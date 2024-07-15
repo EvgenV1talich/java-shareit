df3118154cac3208b72bc388ebfe3b429f32fa88
Evgeny <kharchenko.e.v@yandex.ru>
2024-07-15 12:36:48 +0300

    feat: base describe controller methods

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -1,7 +1,14 @@
 package ru.practicum.shareit.item;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.shareit.item.dto.ItemDto;
+
+import java.util.List;
 
 /**
  * TODO Sprint add-controllers.
@@ -9,4 +16,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/items")
 public class ItemController {
+
+    @PostMapping
+    public void addItem(ItemDto itemDto) {
+
+    }
+    @PatchMapping("/{itemId}")
+    public ItemDto updateItem(@PathVariable int itemId, ItemDto itemDto) {
+        return null;
+    }
+    @GetMapping("/itemId")
+    public ItemDto getItem(@PathVariable int itemId) {
+        return null;
+    }
+    @GetMapping
+    public List<ItemDto> getAllItemsByUser() {
+        //TODO чёто не так с сигнатурой
+        return null;
+    }
+
 }
--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,5 +1,9 @@
 package ru.practicum.shareit.user;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/users")
 public class UserController {
+
+    @PostMapping
+    public void createUser(User user) {
+
+    }
+    @GetMapping
+    public User readUser(Integer id) {
+        return null;
+    }
+    @PatchMapping
+    public User updateUser(User user) {
+        return null;
+    }
+    @DeleteMapping
+    public void deleteUser(Integer id) {
+
+    }
+
+
 }
