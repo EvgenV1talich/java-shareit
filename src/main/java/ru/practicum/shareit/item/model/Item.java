040dc7796e84630a6fc6ba3e4372ef4fdb6f0b51
Evgeny <kharchenko.e.v@yandex.ru>
2024-07-17 09:29:56 +0300

    feat: add Item fields

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,17 @@
 package ru.practicum.shareit.item.model;
 
+import ru.practicum.shareit.user.User;
+
 /**
  * TODO Sprint add-controllers.
  */
 public class Item {
+
+    private int id;
+    private String name;
+    private String description;
+    private String status;
+    private User owner;
+    private String request;
+
 }
