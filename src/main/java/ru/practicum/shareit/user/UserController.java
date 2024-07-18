1b6b46f007f6b5479df0badb35011729fad292b9
Evgeny <kharchenko.e.v@yandex.ru>
2024-07-18 15:02:59 +0300

    feat: add base UserController

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,35 +1,42 @@
 package ru.practicum.shareit.user;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.shareit.user.service.UserService;
 
 /**
  * TODO Sprint add-controllers.
  */
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(path = "/users")
 public class UserController {
 
+    private final UserService service;
+
     @PostMapping
     public void createUser(User user) {
-
+        service.create(user);
     }
+
     @GetMapping
     public User readUser(Integer id) {
-        return null;
+        return service.read(id);
     }
+
     @PatchMapping
-    public User updateUser(User user) {
-        return null;
+    public void updateUser(User user) {
+        service.update(user);
     }
+
     @DeleteMapping
     public void deleteUser(Integer id) {
-
+        service.delete(id);
     }
-
 
 }
