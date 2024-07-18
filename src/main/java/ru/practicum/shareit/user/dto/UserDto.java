d73c995712157e4ff32782c347bc64f6ce83cb8e
Evgeny <kharchenko.e.v@yandex.ru>
2024-07-18 14:37:43 +0300

    fix: remove mapper from UserDto

--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.user.dto;
 
 import lombok.RequiredArgsConstructor;
-import ru.practicum.shareit.user.User;
 
 @RequiredArgsConstructor
 public class UserDto {
@@ -9,8 +8,4 @@ public class UserDto {
     private final String name;
     private final String email;
 
-    public static UserDto userToDto(User user) {
-        return new UserDto(user.getName(),
-                user.getEmail());
-    }
 }
