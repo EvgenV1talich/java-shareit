2dea1199f8e4a9d891cd9f05405bb740e2098c7f
Evgeny <kharchenko.e.v@yandex.ru>
2024-07-18 14:35:20 +0300

    feat: add ItemService logic

--- src/main/java/ru/practicum/shareit/item/service/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemService.java
@@ -1,4 +1,22 @@
 package ru.practicum.shareit.item.service;
 
+import org.springframework.stereotype.Service;
+import ru.practicum.shareit.item.model.Item;
+
+import java.util.List;
+@Service
 public interface ItemService {
+
+    void create(Item item);
+
+    Item read(Integer id);
+
+    Item update(Item item);
+
+    void delete(Integer id);
+
+    List<Item> readAll();
+
+    List<Item> readByUser(Integer userId);
+
 }
--- src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -1,4 +1,42 @@
 package ru.practicum.shareit.item.service;
 
-public class ItemServiceImpl {
+import lombok.RequiredArgsConstructor;
+import ru.practicum.shareit.item.dao.ItemDao;
+import ru.practicum.shareit.item.model.Item;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class ItemServiceImpl implements ItemService {
+
+    private final ItemDao itemDao;
+    @Override
+    public void create(Item item) {
+        itemDao.create(item);
+    }
+
+    @Override
+    public Item read(Integer id) {
+        return itemDao.read(id);
+    }
+
+    @Override
+    public Item update(Item item) {
+        return itemDao.update(item);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        itemDao.delete(id);
+    }
+
+    @Override
+    public List<Item> readAll() {
+        return itemDao.readAll();
+    }
+
+    @Override
+    public List<Item> readByUser(Integer userId) {
+        return itemDao.readByUser(userId);
+    }
 }
