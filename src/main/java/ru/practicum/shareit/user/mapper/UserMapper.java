1f6c6c88404bbc656a2fe1b9ee6c7cba06df04c1
Evgeny <kharchenko.e.v@yandex.ru>
2024-07-29 17:44:42 +0300

    fix: UserMapper backup

--- src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
@@ -1,16 +1,12 @@
 package ru.practicum.shareit.user.mapper;
 
 import org.springframework.stereotype.Component;
-import ru.practicum.shareit.exceptions.UserCreateException;
 import ru.practicum.shareit.user.User;
 import ru.practicum.shareit.user.dto.UserDto;
 
 @Component
 public class UserMapper {
     public static UserDto toDto(User user) {
-        if (user == null) {
-            throw new UserCreateException("Ошибка маппинга пользователя");
-        }
         return new UserDto(user.getName(), user.getEmail());
     }
 
